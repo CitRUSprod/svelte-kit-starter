705924c10605b95bea985191da2af3d9c1835496
CitRUS <citrus4009@gmail.com>
2021-05-16 18:27:14 +0300

    feat: Add bootstrap

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,18 +1,9 @@
 <slot />
 
 <style lang="scss" global>
-    * {
-        font-family: "Arial", "Helvetica", sans-serif;
-    }
-
     :root,
+    body,
     #svelte {
         height: 100%;
-    }
-
-    body {
-        height: 100%;
-        margin: 0;
-        padding: 0;
     }
 </style>
