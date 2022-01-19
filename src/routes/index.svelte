b11e4b23d5f02e879a7b0e875f3faf8e798394d3
CitRUS <citrus4009@gmail.com>
2022-01-19 19:54:52 +0300

    feat: Switch to adapter-auto

--- src/routes/index.svelte
+++
@@ -1,7 +0,0 @@
-<script lang="ts" context="module">
-    import { defaultLocale } from "$lib/locales"
-
-    import type { Load } from "@sveltejs/kit"
-
-    export const load: Load = () => ({ status: 301, redirect: `/${defaultLocale}` })
-</script>
--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,7 @@
 import preprocess from "svelte-preprocess"
 import icons from "unplugin-icons/vite"
 import adapterNode from "@sveltejs/adapter-node"
-import adapterStatic from "@sveltejs/adapter-static"
-
-const supportedLocales = ["en", "ru"]
+import adapterAuto from "@sveltejs/adapter-auto"
 
 /** @type {import("@sveltejs/kit").Config} */
 const config = {
@@ -11,22 +9,7 @@ const config = {
         postcss: true
     }),
     kit: {
-        adapter:
-            process.env.MODE === "ssg"
-                ? adapterStatic({
-                      pages: "dist",
-                      assets: "dist"
-                  })
-                : adapterNode({ out: "dist" }),
-        prerender: {
-            entries: supportedLocales.reduce(
-                (acc, locale) => {
-                    acc.push(`/${locale}`, `/${locale}/404`)
-                    return acc
-                },
-                ["*", "/404"]
-            )
-        },
+        adapter: process.env.ADAPTER === "auto" ? adapterAuto() : adapterNode({ out: "dist" }),
         vite: {
             plugins: [icons({ compiler: "svelte" })]
         }
