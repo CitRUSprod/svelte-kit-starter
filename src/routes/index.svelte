ca0a1c5668bf19a506bcdf9453017c7e4c29a222
CitRUS <citrus4009@gmail.com>
2022-01-19 19:29:55 +0300

    feat: Add main page for redirecting

---
+++ src/routes/index.svelte
@@ -0,0 +1,7 @@
+<script lang="ts" context="module">
+    import { defaultLocale } from "$lib/locales"
+
+    import type { Load } from "@sveltejs/kit"
+
+    export const load: Load = () => ({ status: 301, redirect: `/${defaultLocale}` })
+</script>
