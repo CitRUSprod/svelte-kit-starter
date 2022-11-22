2c9345724e31710338d1cd9a485704c014daab4e
CitRUS <citrus4009@gmail.com>
2022-11-22 23:36:00 +0300

    feat: Remove \\$types.d.ts file

--- src/routes/[locale]/$types.d.ts
+++
@@ -1,8 +0,0 @@
-import type { Load } from "@sveltejs/kit"
-
-export interface LayoutData {
-    route: string
-    [key: string]: unknown
-}
-
-export type LayoutLoad = Load<Record<string, string>, null, LayoutData>
--- src/routes/[locale]/posts/[id=integer]/$types.d.ts
+++
@@ -1,8 +0,0 @@
-import type { Load } from "@sveltejs/kit"
-
-export interface PageData {
-    id: number
-    [key: string]: unknown
-}
-
-export type PageLoad = Load<Record<string, string>, null, PageData>
